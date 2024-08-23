1dcf5d5b0eb9743467673bc95e1324d8a0b0e9e4
tjennings05 <tamlin.jennings@csuglobal.edu>
2024-08-23 13:22:30 -0500

    Add files via upload

---
+++ PersonInfo.java
@@ -0,0 +1,18 @@
+public class PersonInfo
+{
+    public static void main(String[] args)
+    {
+        // Declare variables
+        String firstName = "Bill";
+        String lastName = "Johnson";
+        String streetAddress = "2024 Jones Street";
+        String city = "Nowhere";
+        String zipCode = "54321";
+        // Print information on individual lines
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# PersonInfo1
+# Person.Info
+Critical Thinking 1 - I created a program that displays a persons name and address.
